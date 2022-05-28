03371d00d93697ebbba98c95c38a35b060b2b0fe
celean77777 <celean77777@gmail.com>
2022-05-28 14:25:20 +0300

    Lesson_3

---
+++ src/main/java/com/celean/spring3/services/ProductService.java
@@ -0,0 +1,29 @@
+package com.celean.spring3.services;
+
+import com.celean.spring3.model.Product;
+import com.celean.spring3.repositories.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+@Service
+public class ProductService {
+    private ProductRepository productRepository;
+
+    @Autowired
+    public ProductService(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    public List<Product> findAll() {
+        return productRepository.findAll();
+    }
+
+    public void save(Product product) {
+        productRepository.save(product);
+    }
+
+    public Product findById(Long id) {
+        return productRepository.findById(id);
+    }
+}
